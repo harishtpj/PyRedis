f33b6e60ef48766be6d36b0988d50113f795480e
harishtpj <harishtpj@hotmail.com>
2022-12-28 19:28:35 +0530

    Added PING support

--- app/main.py
+++ app/main.py
@@ -2,8 +2,12 @@ import socket
 
 
 def main():
-    server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    server_socket.accept() # wait for client
+    rserver = socket.create_server(("localhost", 6379), reuse_port=True)
+    rserver.listen(5)
+
+    while True:
+        clnt, c_addr = rserver.accept()
+        clnt.send(b"+PONG\r\n")
 
 
 if __name__ == "__main__":
