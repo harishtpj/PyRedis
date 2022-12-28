03004c65c6f6c1fe0f73b2120272ecd9f192edb6
harishtpj <harishtpj@hotmail.com>
2022-12-28 19:49:32 +0530

    Added PING support with sustain

--- app/main.py
+++ app/main.py
@@ -3,10 +3,9 @@ import socket
 
 def main():
     rserver = socket.create_server(("localhost", 6379), reuse_port=True)
-    rserver.listen(5)
+    clnt, c_addr = rserver.accept()
 
     while True:
-        clnt, c_addr = rserver.accept()
         data = clnt.recv(1024)
         clnt.send(b"+PONG\r\n")
 
