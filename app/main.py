42086f1b57de43acb1f89463cc6d2d6fb2d6555b
harishtpj <harishtpj@hotmail.com>
2023-03-24 19:51:14 +0530

    Added support for handling multiple connections

--- app/main.py
+++ app/main.py
@@ -1,13 +1,21 @@
 import socket
+import threading
 
+def handle_conn(clnt):
+    while True:
+        try:
+            clnt.recv(1024)
+            clnt.send(b"+PONG\r\n")
+        except ConnectionError:
+            break
 
 def main():
     rserver = socket.create_server(("localhost", 6379), reuse_port=True)
     clnt, c_addr = rserver.accept()
 
     while True:
-        data = clnt.recv(1024)
-        clnt.send(b"+PONG\r\n")
+        clnt, c_addr = rserver.accept()
+        threading.Thread(target=handle_conn, args=(clnt,)).start()
 
 
 if __name__ == "__main__":
